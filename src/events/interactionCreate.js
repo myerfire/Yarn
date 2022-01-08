37333abfe4cef9907337aac97cef510ac39cd913
myer <myercheng0614@gmail.com>
2022-01-08 06:46:44 +0000

    tweaks + lastfm client (i think)

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: "interactionCreate",
     active: true,
 	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} of ${interaction.guild.name} triggered an interaction.`);
+		console.log(`[events/interactionCreate.js] [DEBUG] ${interaction.user.tag} in #${interaction.channel.name} of ${interaction.guild.name} triggered an interaction.`);
 	},
 };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 const fs = require("fs");
 const moment = require("moment");
-const { Client, Intents, Collection } = require("discord.js");
-const { Pool } = require("pg");
 const { keys } = require("../config.json");
+const { Client, Intents, Collection } = require("discord.js");
+const LastFm = require("lastfm-node-client");
+const { Pool } = require("pg");
 
 const client = new Client({ intents: new Intents(32767) }); // 32767 is all intents
 /* LOAD COMMANDS */
 client.commands = new Collection();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-console.log("[index.js] Loading commands..");
+console.log("[index.js] [COMMANDS] Loading commands..");
 for (const commandFile of commandFiles) {
     const command = require(`./commands/${commandFile}`);
     if (!command.enabled) continue;
     client.commands.set(command.data.name, command);
-    console.log(`[index.js] Command /${command.data.name} loaded.`);
+    console.log(`[index.js] [COMMANDS] Command /${command.data.name} loaded.`);
 }
 /* REGISTER EVENTS */
 const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
-console.log("[index.js] Registering events..");
+console.log("[index.js] [EVENTS] Registering events..");
 for (const eventFile of eventFiles) {
     const event = require(`./events/${eventFile}`);
     if (!event.active) continue;
     if (event.once) {
         client.once(event.name, (...args) => event.execute(...args));
-        console.log(`[index.js] One time event ${event.name} registered.`);
+        console.log(`[index.js] [EVENTS] One time event ${event.name} registered.`);
     } else {
         client.on(event.name, (...args) => event.execute(...args));
-        console.log(`[index.js] Persistent event ${event.name} registered.`);
+        console.log(`[index.js] [EVENTS] Persistent event ${event.name} registered.`);
     }
 }
 /* COMMAND EXECUTION */
@@ -47,5 +48,7 @@ client.db = new Pool({
     database: "yarn",
     password: keys.postgres,
 });
-
+/* LAST.FM */
+client.lastfm = new LastFm(keys.lastfm);
+/* START BOT */
 client.login(keys.token);
